826ea6e4fcacac474f1672bf5e651c7adfba05df
mnmndevlol <mnmndevlol@gmail.com>
2022-10-28 22:34:43 +0900

    aa

--- components/Navigator.js
+++ components/Navigator.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
+
 const Navigator = () => {
   return (
     <div class="nav">
-      <a href="/">HOME</a> |<a href="about">ABOUT</a> |<a href="board">BOARD</a>{" "}
-      |<a href="article">ARTICLE</a> |<a href="subscription">SUBSCRIPTION</a> |
-      <a href="mypage">MYPAGE </a> |<a href="logout">LOGOUT</a>
+      <Link href="/">HOME</Link> |<Link href="about">ABOUT</Link> |
+      <Link href="board">BOARD</Link> |<Link href="article">ARTICLE</Link> |
+      <Link href="subscription">SUBSCRIPTION</Link> |
+      <Link href="mypage">MYPAGE </Link> |<Link href="logout">LOGOUT</Link>
     </div>
   );
 };
